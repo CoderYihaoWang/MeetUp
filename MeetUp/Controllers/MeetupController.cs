2538ca19d5b43d844a3e37b17eaf1b121cff7ad1
CoderYihaoWang <yesiamyihao@gmail.com>
2020-09-12 23:01:53 +1200

    Create meetup controller

---
+++ MeetUp/Controllers/MeetupController.cs
@@ -0,0 +1,26 @@
+﻿using MeetUp.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetUp.Controllers
+{
+    [Route("api/meetup")]
+    public class MeetupController : ControllerBase
+    {
+        private MeetupContext _meetupContext;
+
+        public MeetupController(MeetupContext meetupContext)
+        {
+            _meetupContext = meetupContext;
+        }
+        
+        [HttpGet]
+        public ActionResult<List<Meetup>> Get()
+        {
+            return _meetupContext.Meetups.ToList();
+        }
+    }
+}
