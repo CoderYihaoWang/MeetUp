846f2896f7e60b47ced22fa1350cd499197fdd44
CoderYihaoWang <yesiamyihao@gmail.com>
2020-09-12 16:07:08 +1200

    Add entities

---
+++ MeetUp/Entity/Meetup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetUp.Entity
+{
+    public class Meetup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Organizer { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsPrivate { get; set; }
+
+        // virtual is for lazy loading, which uses proxy
+        public virtual Location Location { get; set; }
+        public virtual List<Lecture> Lectures { get; set; }
+    }
+}
