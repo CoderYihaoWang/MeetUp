5ea9d74e61a74876060847132bfc4ff3980caf21
CoderYihaoWang <yesiamyihao@gmail.com>
2020-09-12 16:28:58 +1200

    Add DbContext; configure relations

--- MeetUp/Entity/Lecture.cs
+++ MeetUp/Entity/Lecture.cs
@@ -9,7 +9,7 @@ namespace MeetUp.Entity
     {
         public int Id { get; set; }
         public string Author { get; set; }
-        public string Subject { get; set; }
+        public string Topic { get; set; }
         public string Description { get; set; }
 
         public virtual Meetup Meetup { get; set; }
