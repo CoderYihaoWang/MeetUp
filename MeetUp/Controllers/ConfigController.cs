4d5a7a5ce7007482d75bbb45b9c87df38f7ca8d9
CoderYihaoWang <yesiamyihao@gmail.com>
2020-09-15 16:08:13 +1200

    Add reload action

---
+++ MeetUp/Controllers/ConfigController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetUp.Controllers
+{
+    [Route("config")]
+    public class ConfigController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpOptions("reload")]
+        public ActionResult Reload()
+        {
+            try
+            {
+                ((IConfigurationRoot)_configuration).Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
